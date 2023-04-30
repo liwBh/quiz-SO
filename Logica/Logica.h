6f8c77aa6ca9192ae5edc7c6b2a56557a7cf6912
Wilfredo BH <68663454+liwBh@users.noreply.github.com>
2023-04-30 17:09:17 -0600

    correcion de saturacion de memoria al desfragmentar

--- Logica/Logica.h
+++ Logica/Logica.h
@@ -83,6 +83,46 @@ void desfragmentarMemoria(struct Bloque matriz[8][8], ListaProcesos *listaConten
    // mostrarMatriz(matriz);
 }
 
+
+
+NodoProcesos* seleccionarNodoAleatorio(ListaProcesos *listaContenedor) {
+    // Obtener el número de nodos de la lista
+    int nNodos = 0;
+    NodoProcesos *actual = listaContenedor->primero;
+    while (actual != NULL) {
+        nNodos++;
+        actual = actual->siguiente;
+    }
+
+    // Generar un número aleatorio en el rango de 0 a n-1
+    int indiceAleatorio = rand() % nNodos;
+
+    // Recorrer la lista y detenerse en el nodo correspondiente al número aleatorio generado
+    actual = listaContenedor->primero;
+    for (int i = 0; i < indiceAleatorio; i++) {
+        actual = actual->siguiente;
+    }
+
+    return actual;
+}
+
+//Metodo para liberar memoria de la matriz segun las posiciones en que se ubica el proceso, con la lista de posiciones
+void liberarMemoria(NodoProcesos *nodo, struct Bloque matriz[8][8]){
+    NodoPosicion *aux = nodo->listaPosicion->primero;
+
+    //liberar bloques de matriz
+    while (aux != NULL){ //Se recorre la lista de posiciones del proceso
+
+        matriz[aux->i][aux->j].disponible = 0; //Y se libera la disponibilidad en memoria
+        matriz[aux->i][aux->j].idProceso = 0; //Se libera el id del proceso en memoria
+
+        //printf("Eliminando pos i:%d j:%d ",);
+
+        aux = aux->siguiente;
+    }
+
+}
+
 //Metodo que recorra matriz y busque el primer espacio disponible, devuelve i y j
 ListaPosicion *asignarEspacioDisponible(struct Bloque matriz[8][8], NodoProcesos *nodo, ListaProcesos *listaContenedor){
 
@@ -128,102 +168,14 @@ ListaPosicion *asignarEspacioDisponible(struct Bloque matriz[8][8], NodoProcesos
             aux = aux->siguiente;
         }
     }else{
-        //defragmentar
+        //liberar un proceso para evitar saturacion de memoria
+        NodoProcesos *nodoEliminar = seleccionarNodoAleatorio(listaContenedor); // Almacenar nodo a eliminar
+        printf("\nLiberando de memoria %d bloques del proceso: %d",(nodoEliminar->peso/4), nodoEliminar->id);
+        liberarMemoria(nodoEliminar,matriz); //Libera la memoria por medio de la lista de posiciones que tiene el procesoodoEl
 
+        //defragmentar
         printf("\n\n*****Es necesario desfragmentar memoria*****\n");
         desfragmentarMemoria(matriz,listaContenedor);
-    }
-
-}
-
-/*
-void asignarEspacioDisponible(struct Bloque matriz[8][8], NodoProcesos *nodo){
-
-    // determinar el numero de bloques del proceso
-    int nBloques = (nodo->peso / 4);
-
-    printf("El numero de bloques: %d para el proceso: %d", nBloques, nodo->id);
-
-    for (int i = 0; i < 8; i++) {
-        for (int j = 0; j < 8; j++) {
-
-            if(matriz[i][j].disponible == 0 ) {
-                //insertar i,j en un nodo posicion
-                NodoPosicion *nodoPosicion = crearNodoPosicion(i, j);
-
-                matriz[i][j].disponible = 1;
-                matriz[i][j].idProceso = nodo->id;
-
-                //agregar la posicion en la lista del proceso
-                insertarNodoPosicion(nodo->listaPosicion, nodoPosicion);
-
-                nBloques--;
-
-                if (nBloques == 0) {
-                    i = 8;
-                    break;
-                }
-
-            }
-        }
-
-    }
-}
-
-*/
-
-NodoProcesos* seleccionarNodoAleatorio(ListaProcesos *listaContenedor) {
-    // Obtener el número de nodos de la lista
-    int nNodos = 0;
-    NodoProcesos *actual = listaContenedor->primero;
-    while (actual != NULL) {
-        nNodos++;
-        actual = actual->siguiente;
-    }
-
-    // Generar un número aleatorio en el rango de 0 a n-1
-    int indiceAleatorio = rand() % nNodos;
-
-    // Recorrer la lista y detenerse en el nodo correspondiente al número aleatorio generado
-    actual = listaContenedor->primero;
-    for (int i = 0; i < indiceAleatorio; i++) {
-        actual = actual->siguiente;
-    }
-
-    return actual;
-}
-//void moverUnosAlFinal(int matriz[8][8]) {
-//    int ultimoCero = 7;
-//    for (int i = 7; i >= 0; i--) {
-//        for (int j = 7; j >= 0; j--) {
-//            if (matriz[i][j] == 1) {
-//                while (ultimoCero >= 0 && matriz[ultimoCero][j] == 1) {
-//                    ultimoCero--;
-//                }
-//                if (ultimoCero >= 0) {
-//                    matriz[ultimoCero][j] = 1;
-//                    matriz[i][j] = 0;
-//                    ultimoCero--;
-//                }
-//            }
-//        }
-//    }
-//}
-
-
-//Metodo para liberar memoria de la matriz segun las posiciones en que se ubica el proceso, con la lista de posiciones
-void liberarMemoria(NodoProcesos *nodo, struct Bloque matriz[8][8]){
-    NodoPosicion *aux = nodo->listaPosicion->primero;
-
-    //liberar bloques de matriz
-    while (aux != NULL){ //Se recorre la lista de posiciones del proceso
-
-        matriz[aux->i][aux->j].disponible = 0; //Y se libera la disponibilidad en memoria
-        matriz[aux->i][aux->j].idProceso = 0; //Se libera el id del proceso en memoria
-
-        //printf("Eliminando pos i:%d j:%d ",);
-
-        aux = aux->siguiente;
     }
 
 }
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/jurguen/Documentos/GitHub/quiz-SO
+# Install script for directory: /home/liwbh/CLionProjects/quiz-SO
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/jurguen/Documentos/GitHub/quiz-SO/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/liwbh/CLionProjects/quiz-SO/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
